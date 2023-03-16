d73c8b29c262e7cbdc749c86c52a9c96118f8441
Jakub <jakub.diversa@wp.pl>
2023-03-16 12:10:36 +0100

    few change

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-    'https://yoursite.pl', 
-    'http://127.0.0.1:5500', 
-    'http://localhost:3500'
+   "https://yoursite.pl",
+   "http://127.0.0.1:5500",
+   "http://localhost:3500",
+   "http://localhost:3000",
 ];
 
 module.exports = allowedOrigins;
